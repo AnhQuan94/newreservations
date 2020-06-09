105097298768c86913ee37baafbf0e2c695d13fb
Anh <59912241+AnhQuan94@users.noreply.github.com>
2020-06-09 22:48:50 +0200

    Modèle Location +Relation ManyToOne with Locality

--- src/DataFixtures/LocalityFixtures.php
+++ src/DataFixtures/LocalityFixtures.php
@@ -3,44 +3,32 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Locality;
-
 
 class LocalityFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         $localities = [
-            [
-                'locality'=>'Bruxelles',
-                'postal_code'=>'1000'
-            ],
-            [
-                'locality'=>'Laeken',
-                'postal_code'=>'1020'
-            ],
-            [
-                'locality'=>'Schaerbeek',
-                'postal_code'=>'1030'
-            ],
-            [
-                'locality'=>'Ixelles',
-                'postal_code'=>'1050'
-            ],
-            [
-                'locality'=>'Watermael-Boitsfort',
-                'postal_code'=>'1170'
-            ],
+            ['postal_code'=>'1000','locality'=>'Bruxelles'],
+            ['postal_code'=>'1020','locality'=>'Laeken'],
+            ['postal_code'=>'1030','locality'=>'Schaerbeek'],
+            ['postal_code'=>'1050','locality'=>'Ixelles'],
+            ['postal_code'=>'1070','locality'=>'Anderlecht'],
+            ['postal_code'=>'1180','locality'=>'Uccle'],
+            ['postal_code'=>'1170','locality'=>'Watermael-Boitsfort'],
         ];
+        
         foreach ($localities as $record) {
             $locality = new Locality();
-            $locality->setLocality($record['locality']);
             $locality->setPostalCode($record['postal_code']);
-
+            $locality->setLocality($record['locality']);
             $manager->persist($locality);
+            
+            $this->addReference($record['locality'], $locality);
         }
-
+        
         $manager->flush();
     }
 }
