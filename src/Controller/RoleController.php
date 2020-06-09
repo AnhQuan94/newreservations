4f211ebcd1d4c41ae97cae7372767e4e78710817
Anh <59912241+AnhQuan94@users.noreply.github.com>
2020-06-09 21:31:36 +0200

    Models, controllers, templates, routes for Type, Locality and Role

---
+++ src/Controller/RoleController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Role;
+
+class RoleController extends AbstractController
+{
+    /**
+     * @Route("/role", name="role")
+     */
+  /**
+     * @Route("/role", name="role")
+     */
+    public function index()
+    {
+        $repository = $this->getDoctrine()->getRepository(Role::class);
+        $roles = $repository->findAll();
+        
+        return $this->render('role/index.html.twig', [
+            'roles' => $roles,
+            'resource' => 'roles',
+        ]);
+    }
+
+    /**
+     * @Route("/role/{id}", name="role_show")
+     */
+    public function show($id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Role::class);
+        $role = $repository->find($id);
+
+        return $this->render('role/show.html.twig', [
+            'role' => $role,
+        ]);
+    }
+}
+
+
